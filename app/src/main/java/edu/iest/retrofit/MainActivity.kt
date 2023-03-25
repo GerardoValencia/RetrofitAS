1e83daafa2a18422c4de006fbf101c505936ca0d
GerardoValencia <gerardo.valencia@iest.edu.mx>
2023-03-24 19:05:57 -0600

    24/03/23-IEST

---
+++ app/src/main/java/edu/iest/retrofit/MainActivity.kt
@@ -0,0 +1,55 @@
+package edu.iest.retrofit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import edu.iest.retrofit.models.ImageRandom
+import edu.iest.retrofit.network.API
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        traerImagenAleatoria()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_images, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.option_menu_list_images){
+            Toast.makeText(this, "OPTION menu 1",
+            Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun traerImagenAleatoria() {
+        val apiCall = API().crearServicioAPI()
+
+        apiCall.imagenAleatoria().enqueue(object : Callback<ImageRandom> {
+            override fun onResponse(call: Call<ImageRandom>, response: Response<ImageRandom>){
+                Log.d("API_PRUEBA", "status es " + response.body()?.status)
+                Log.d("API_PRUEBA", "message es " + response.body()?.message)
+                // Aquí hacer lo que necesitemos con los datos
+            }
+
+            override fun onFailure(call: Call<ImageRandom>, t: Throwable) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "No fue posible conectar a API",
+                    Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+}
---
+++ app/src/main/java/edu/iest/retrofit/network/ApiInterface.kt
@@ -0,0 +1,18 @@
+package edu.iest.retrofit.network
+
+import edu.iest.retrofit.models.ImageRandom
+import edu.iest.retrofit.models.ListBreed
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+
+interface ApiInterface {
+    @GET("breeds/image/random")
+    fun imagenAleatoria(): Call<ImageRandom>
+
+    @GET("breed/{raza}/images")
+    fun listaImagenesDePerrosPorRaza(@Path("raza") raza: String): Call<ListBreed>
+    //si raza = "chihuaua" url seria breed/chiuaua/images
+    //si raza es = "pastor" url seria breed/pastor/images
+}
